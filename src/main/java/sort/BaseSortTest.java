0c24dc49fb9f977354e541ab4d4212b7c7b4a606
邓超（晋辉科技） <2391876660@qq.com>
2022-07-25 10:33:24 +0800

    选择排序

--- src/main/java/sort/BaseSortTest.java
+++ src/main/java/sort/BaseSortTest.java
@@ -1,5 +1,7 @@
 package sort;
 
+import com.sun.xml.internal.bind.v2.model.annotation.Quick;
+
 import java.util.Arrays;
 
 /**
@@ -14,8 +16,8 @@ public class BaseSortTest {
 
 
     public static void main(String[] args) {
-        int arr[] = new int[]{21, 2, 1, 31, 10, 0};
-        mergeSort(arr);
+        int arr[] = new int[]{3, 1, 7, 5, 4};
+        quickSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -115,18 +117,20 @@ public class BaseSortTest {
     }
 
     /**
-     * 归并排序
+     * 归并排序，分而治之的思想，先分，再合并
+     * 时间复杂度 O(nlogn)
+     *
      * @param arr
      */
     public static void mergeSort(int[] arr) {
         int len = arr.length;
-        int mid = len / 2;
         part(0, len - 1, arr);
     }
 
 
     /**
      * 分的动作
+     *
      * @param left
      * @param right
      * @param arr
@@ -147,6 +151,7 @@ public class BaseSortTest {
 
     /**
      * 合的动作
+     *
      * @param left
      * @param mid
      * @param right
@@ -159,6 +164,7 @@ public class BaseSortTest {
         int[] temp = new int[right - left + 1];
         //临时数组的初始化指针
         int k = 0;
+        //对比左右两部分每个元素的大小，分别插入到临时数组
         while (l <= mid && r <= right) {
             if (arr[l] > arr[r]) {
                 temp[k++] = arr[r++];
@@ -166,15 +172,94 @@ public class BaseSortTest {
                 temp[k++] = arr[l++];
             }
         }
+        // 左边部分剩余的元素插入
         while (l <= mid) {
             temp[k++] = arr[l++];
         }
+        //右边部分剩余的元素插入
         while (r <= right) {
             temp[k++] = arr[r++];
         }
         k = 0;
+        //将排序好的元素放回到原数组
         for (int i = left; i <= right; i++) {
             arr[i] = temp[k++];
+        }
+    }
+
+    /**
+     * 快速排序
+     * 时间复杂度 O(nlogn)
+     * 21, 2, 1, 31, 10, 0
+     *
+     * @param arr
+     */
+    public static void quickSort(int[] arr) {
+        quickSortPointsTwo(arr, 0, arr.length - 1);
+    }
+
+    /**
+     * 快速排序2-优化后的，快排1可能存在元素重复位移的情况
+     *
+     * @param arr
+     * @param left
+     * @param right
+     */
+    public static void quickSortPointsTwo(int[] arr, int left, int right) {
+        if (left < right) {
+            //定义中间元素，最左边最右边或者中间都可以
+            int mid = arr[left];
+            //两个指针，指向左右两边的元素
+            int l = left + 1, r = right;
+            while (true) {
+                while (l <= r && arr[l] <= mid) {
+                    l++;
+                }
+                while (l <= r && arr[r] >= mid) {
+                    r--;
+                }
+                if (l >= r) {
+                    break;
+                }
+                int temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+            }
+            arr[left] = arr[r];
+            arr[r] = mid;
+            quickSortPointsTwo(arr, left, r - 1);
+            quickSortPointsTwo(arr, r + 1, right);
+
+        }
+    }
+
+    /**
+     * 快速排序1
+     *
+     * @param arr
+     * @param left
+     * @param right
+     */
+    public static void quickSortPoints(int[] arr, int left, int right) {
+        if (left < right) {
+            //选取出中间元素
+            int mid = arr[right];
+            //定义两个指针a,b，a指针指向当前大于right的元素，b指针用来遍历数组，初始化的时候都从left开始
+            int b = left, a = left;
+            for (; b <= right; b++) {
+                if (arr[b] < mid) {
+                    //交换a、b两个元素的位置
+                    int temp = arr[b];
+                    arr[b] = arr[a];
+                    //a+1将指针指向下一个元素
+                    arr[a++] = temp;
+                }
+            }
+            //将中间元素与a元素交换，a元素的左边是大于中间元素的，a元素的右边是小于中间元素的
+            arr[right] = arr[a];
+            arr[a] = mid;
+            quickSortPoints(arr, left, a - 1);
+            quickSortPoints(arr, a + 1, right);
         }
     }
 
